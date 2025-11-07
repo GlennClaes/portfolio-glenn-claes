5e1569f6c3a767accf2b48be983ced517cbfa88c
GlennClaesPXL <148762236+GlennClaesPXL@users.noreply.github.com>
2025-11-07 11:34:40 +0100

    Fix CD

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,10 +7,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     vueJsx(),
-    vueDevTools(),
   ],
   resolve: {
     alias: {
