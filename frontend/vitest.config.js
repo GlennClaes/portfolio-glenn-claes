1b6d4fb47b07ae1ff0b8d6c3f5a62887306dabf6
GlennClaesPXL <Glenn.Claes@student.pxl.be>
2025-11-26 09:22:05 +0100

    tests

--- frontend/vitest.config.js
+++ frontend/vitest.config.js
@@ -1,3 +1,4 @@
+// vitest.config.js
 import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
@@ -9,6 +10,17 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
+
+      coverage: {
+        provider: 'istanbul',
+        reporter: ['html', 'text-summary'],
+        all: true,
+        include: ['src/**/*.{js,ts,vue}'],
+        lines: 100,
+        functions: 100,
+        branches: 100,
+        statements: 100,
+      }
     },
   }),
 )
